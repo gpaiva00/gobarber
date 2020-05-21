881248305bd09f75194cc3698ecd5a957cde2f37
gabriel paiva <gab@MacBook-Pro-de-gabriel.local>
2020-05-21 20:11:54 -0300

    Removed wrong folders

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, {FC, ButtonHTMLAttributes} from 'react';
-
-import { Container } from './styles'
-
-type ButtonProps =  ButtonHTMLAttributes<HTMLButtonElement>
-
-const Button: FC<ButtonProps> = ({ children, ...props }) => (<Container><button type="button" {...props}>{children}</button></Container>)
-
-export default Button;
